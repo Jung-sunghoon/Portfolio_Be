cb712dd01f5bec9b0c0b80236ef6927f586d6eeb
JShoon <jsh021813@gmail.com>
2023-12-21 16:45:21 +0900

    feat: node 구동 환경 설치

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+// index.js
+
 const express = require("express");
+const mariadb = require("mariadb");
 
 const app = express();
+const port = 8080;
 
-app.listen(8080, function () {
-  console.log("server start 8080");
+// MariaDB 연결 풀 설정
+const pool = mariadb.createPool({
+  host: "localhost",
+  user: "portfolio",
+  password: "wjdtjd1787!",
+  database: "portfolio",
+  connectionLimit: 10, // 연결 풀의 최대 연결 수
+  acquireTimeout: 30000, // 연결을 얻어오는데 허용되는 최대 시간 (밀리초)
+  allowPublicKeyRetrieval: true, // RSA 공개 키 검색 허용
+});
+
+// 루트 엔드포인트
+app.get("/", (req, res) => {
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query("SELECT * FROM BlogPost")
+        .then((results) => {
+          res.json(results);
+        })
+        .catch((err) => {
+          console.error("Error querying database:", err);
+          res.status(500).json({ error: "Internal Server Error" });
+        })
+        .finally(() => {
+          conn.release();
+        });
+    })
+    .catch((err) => {
+      console.error("Error connecting to MariaDB:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
