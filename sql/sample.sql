4e6acb26edcf158e4ed70f534609f4429ad8d6f0
JShoon <jsh021813@gmail.com>
2024-01-15 21:29:45 +0900

    feat: sql INSERT 문 추가

--- sql/sample.sql
+++ sql/sample.sql
@@ -10,8 +10,8 @@ CREATE TABLE BlogPost (
 CREATE TABLE CalendarEvent ( 
   event_id INT AUTO_INCREMENT PRIMARY KEY, 
   event_title VARCHAR(255) NOT NULL, 
-  start_time DATETIME NOT NULL, 
-  end_time DATETIME NOT NULL, 
+  start_time DATETIME2 NOT NULL, 
+  end_time DATETIME2 NOT NULL, 
   event_text LONGTEXT, 
   event_type varchar(255) NOT NULL,
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -22,3 +22,15 @@ CREATE TABLE User (
   email VARCHAR(255) NOT NULL PRIMARY KEY,
   username VARCHAR(255) NOT NULL
 );
+
+INSERT INTO USER ( email, username) VALUES ('jsh021813@gmail.com', '정성훈');
+
+INSERT INTO BlogPost ( title, content) VALUES ('테스트용 게시물 1', '안녕하세요 블로그 게시물 1번입니다.');
+INSERT INTO BlogPost ( title, content) VALUES ('테스트용 게시물 2', '안녕하세요 블로그 게시물 2번입니다.');
+INSERT INTO BlogPost ( title, content) VALUES ('테스트용 게시물 3', '안녕하세요 블로그 게시물 3번입니다.');
+
+INSERT INTO CalendarEvent ( event_title, start_time, end_time, event_type) VALUES ('이벤트 1', '2024-01-01 00:00:00', '2024-01-01 00:01:01', '중요')
+INSERT INTO CalendarEvent ( event_title, start_time, end_time, event_type) VALUES ('이벤트 2', '2024-01-02 00:00:00', '2024-01-02 00:02:01', '매우 중요')
+INSERT INTO CalendarEvent ( event_title, start_time, end_time, event_type) VALUES ('이벤트 3', '2024-01-03 00:00:00', '2024-01-04 00:03:01', '평범')
+INSERT INTO CalendarEvent ( event_title, start_time, end_time, event_type) VALUES ('이벤트 4', '2024-01-04 00:00:00', '2024-01-04 00:04:01', '중요')
+INSERT INTO CalendarEvent ( event_title, start_time, end_time, event_type) VALUES ('이벤트 5', '2024-01-05 00:00:00', '2024-01-05 00:05:01', '매우 중요')
