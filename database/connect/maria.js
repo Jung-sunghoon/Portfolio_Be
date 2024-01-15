dae4258abc0f2c89c5e90bbb399172d27e4f9084
JShoon <jsh021813@gmail.com>
2024-01-15 22:17:24 +0900

    feat: maria.js port 추가

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -10,6 +10,7 @@ const pool = mariadb.createPool({
   user: process.env.PORTFOLIO_DB_USER,
   password: process.env.PORTFOLIO_DB_PASSWORD,
   database: process.env.PORTFOLIO_DB_DATABASE,
+  port: process.env.PORTFOLIO_DB_PORT,
   connectionLimit: 10, // 연결 풀의 최대 연결 수
   acquireTimeout: 30000, // 연결을 얻어오는데 허용되는 최대 시간 (밀리초)
   allowPublicKeyRetrieval: true, // RSA 공개 키 검색 허용
