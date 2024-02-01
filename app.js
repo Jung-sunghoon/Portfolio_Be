0b986858f02dd6ce35ab81f8d051ea7a0126d7fc
JShoon <jsh021813@gmail.com>
2024-02-01 17:42:43 +0900

    fix: cors 도메인 전체 허용

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
